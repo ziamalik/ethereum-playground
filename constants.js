2e30116030eb2c8fc32ff9c66298fcd907c9d3ce
Zia Malik <ziamalik@gmail.com>
2019-07-04 18:54:14 -0400

    Added Separate constants for Main network

--- constants.js
+++ constants.js
@@ -1,6 +1,10 @@
 module.exports = {
-    //Ethereum Network URL (e.g. Get it from Infura)
-    ETH_URL: "UPDATE_THIS",
-    //Etheruem Account Addesss
-    ETH_ADDRESS: "UPDATE_THIS"
+    //Ethereum Main Network URL (e.g. Get it from Infura)
+    MAIN_ETH_URL: "UPDATE_THIS",
+    //Etheruem Account Addesss - Main Network
+    MAIN_ETH_ADDRESS: "UPDATE_THIS",
+    //Ethereum Test Network URL (e.g. Get it from Infura)
+    TEST_ETH_URL: "UPDATE_THIS",
+    //Etheruem Account Addesss - Test Network
+    TEST_ETH_ADDRESS: "UPDATE_THIS"
 };
--- index.js
+++ index.js
@@ -2,10 +2,23 @@ var Web3 = require('web3');
 var constants = require("./constants");
 
 //Make sure you update the constant.js file
-var web3 = new Web3(constants.ETH_URL);
+var web3 = new Web3(constants.TEST_ETH_URL);
 
 //Print out the account balance in Wei
-web3.eth.getBalance(constants.ETH_ADDRESS, (err, bal) => { 
-    console.log(bal); 
+web3.eth.getBalance(constants.TEST_ETH_ADDRESS, (err, bal) => { 
+    console.log("Balance:" + bal + " Wei"); 
 });
 
+
+//Print out the account balance in Ether
+web3.eth.getBalance(constants.TEST_ETH_ADDRESS, (err, bal) => { 
+    ether = web3.utils.fromWei(bal, 'ether')
+    console.log("Balance:" + ether + " Ether"); 
+});
+
+
+//Get the transaction count
+web3.eth.getTransactionCount(constants.TEST_ETH_ADDRESS).then(count => {
+    console.log("Transaction Count: " + count);
+
+});
