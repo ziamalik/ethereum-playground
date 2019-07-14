e41c09ad97d4ce54b1c22c0f70ef6d2548a0115a
Zia Malik <ziamalik@gmail.com>
2019-07-14 17:54:55 -0400

    Minor fixes

--- simple-message.sol
+++ simple-message.sol
@@ -2,11 +2,9 @@ pragma solidity ^0.5.10;
 
 contract SimpleMessage {
     string myMessage;
-    
     function setMessage(string memory x) public {
         myMessage = x;
     }
-    
     function getMessage() public view returns (string memory) {
         return myMessage;
     }
