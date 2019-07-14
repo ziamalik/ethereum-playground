0dd2f2f04209fe577b1137bfe7c29fff0597710e
Zia Malik <ziamalik@gmail.com>
2019-07-14 11:19:22 -0400

    Testing to send transactions locally to Ganache

--- local.js
+++ local.js
@@ -1,5 +1,5 @@
 var Web3 = require('web3');
-var EthereumTransaction = require("ethereumjs-tx");
+var EthereumTransaction = require("ethereumjs-tx").Transaction;
 
 // Local Ethereum URL with Ganache GUI 
 var url = 'HTTP://127.0.0.1:7545';
@@ -23,3 +23,32 @@ web3.eth.getBalance(sendingAddress).then(sb => {
 web3.eth.getBalance(receivingAddress).then(rb => {
     console.log("Receiving Account Balance: " + rb);
 });
+
+//Get the Gas Price in the network
+web3.eth.getGasPrice().then(p => {console.log("Gas price: " + p);});
+
+var rawTransaction = { 
+    nonce: 0, 
+    to: receivingAddress, 
+    gasPrice: 2000, 
+    gasLimit: 30000, 
+    value: 1000000000000000000, 
+    data: "" 
+};
+
+//Signing the transactions
+var privateKeySender = 'ed03d5e3799e759abd8ceb303592a7fb0dbe5912db115ff57625fd901ded9357';
+var privateKeySenderHex = new Buffer(privateKeySender, 'hex');
+var transaction = new EthereumTransaction(rawTransaction);
+transaction.sign(privateKeySenderHex);
+
+//Sending the transaction to network
+var serializedTransaction = transaction.serialize();
+web3.eth.sendSignedTransaction(serializedTransaction).then(receipt => console.log("Transaction receipt: ", receipt));
+
+
+
+
+
+
+
